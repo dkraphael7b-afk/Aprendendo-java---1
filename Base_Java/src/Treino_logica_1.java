4eba335261c1be8ad4714ac42cab76bff1715e54
dkraphael7b-afk <derick.raphael7b@gmail.com>
2026-04-14 08:14:33 -0300

    bom, eu apliquei a validação de input, melhorei a estrutura, e compri com a regra de negocio sem passar do que foi pedido mas melhorando o codigo dentro do permitido...

--- Base_Java/src/Treino_logica_1.java
+++ Base_Java/src/Treino_logica_1.java
@@ -3,46 +3,46 @@ import java.util.Scanner;
 public class Treino_logica_1 {
     public static void main(String[] args) {
 
+        //Codigo melhorado...
+
         int idade;
-        double salario;
         int score;
-        double valor_parcelas;
-        double valor_emprestimo;
-        double parcelas;
-        double parcelas2;
-        Scanner leitor = new Scanner(System.in);
+        double salario;
+        Scanner leia = new Scanner(System.in);
 
-        System.out.println("Idade do cliente: ");
-        idade = leitor.nextInt();
+        System.out.println("Idade do usuario: ");
+        idade = leia.nextInt();
 
-        if (idade >= 18) {
-            System.out.println("Idade autorizada");
-            System.out.println("Salario do cliente: ");
-            salario = leitor.nextDouble();
-
-            System.out.println("Score do cliente: ");
-            score = leitor.nextInt();
-
-            if (score >= 700 && salario >= 1500.00) {
-                System.out.print("Emprestimo aprovado");
-            } else if ((score >= 500) && (score <= 699) && (salario >= 5000.00)) {
-                System.out.println("Emprestimo aprovado");
-            } else {
-                System.out.println("Emprestimo não aprovado...score ou salario baixo");
-                return; // outro "pare" , mas esse é para condições
+        // validação de dado
+        if (idade < 0) {
+            System.out.println("Dado invalido...");
+            return;
             }
 
-            valor_parcelas = salario * 0.30;
-            System.out.println("Digite o valor do emprestimo");
-            valor_emprestimo = leitor.nextDouble();
-            parcelas = (int) (valor_emprestimo / valor_parcelas);
-            parcelas2 = (valor_emprestimo % valor_parcelas);
-            System.out.println(parcelas + " de " + valor_parcelas + "R$");
-            System.out.println("E +1 parcela de " + parcelas2 + "R$");
-            System.out.println("Agradecemos a preferência...");
-        } else {
-            System.out.print("Idade não autorizada...");
-        }
+        if (idade >= 18) {
+            System.out.println("Idade aprovada: ");
+            //Adicionando o score
+            System.out.println("Digite o Score: ");
+            score = leia.nextInt();
 
+            //Adicionando salario
+            System.out.println("Digite o salario do usuario: ");
+            salario = leia.nextDouble();
+
+            //validação de dados
+            if (salario <= 0.0) {
+                System.out.println("Dado invalido...");
+                return;
+            }
+
+            if (score >= 700 && score <= 1000 && salario >= 1500.00) {
+                System.out.println("Emprestimo aprovado!!!");
+            } else if (score >= 500 && score <= 699 && salario >= 5000.00) {
+                System.out.println("Emprestimo aprovado!!!");
+            } else {
+                System.out.println("Emprestimo não aprovado, score ou salario baixo!!!");
+            }
+
+        }
     }
 }
